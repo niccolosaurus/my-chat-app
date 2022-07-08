9b50dc9e1e8f7b33e27c31f16629c141031a0d51
Niccolo Eck <nicco.eck@gmail.com>
2022-07-07 22:34:20 -0700

    Removed some of the boiler plate React code, and started adding in my own code

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          This will be my chat app. Not sure what to call it or where to start, still learning how to do this.
       </header>
     </div>
   );
